523b5066606c73d653261266e12109c06f302881
Michael Fahim <michael_fahim@yahoo.com>
2020-03-03 01:49:10 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,16 @@
 require_relative '../config/environment'
 
-gem Hashie
-gem Sinatra
-gem Octokit
-gem Pry
-gem AwesomePrint
+source "https://rubygems.org"
+gem "sinatra", "1.4.4"
+gem "hashie"
+gem "octokit", "~> 2.0"
+
+gem "awesome_print", :git => "git@github.com:awesome-print/awesome_print.git"
+
+group :development do
+gem "pry"
+end
+
+group :test do
+gem "rspec"
+end 
